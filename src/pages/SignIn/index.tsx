d8ea4b10607c9a621b22fbc91492611dd9f403b3
choiseona <tjsdk617@naver.com>
2025-02-05 05:02:17 +0900

    feat: 로그인 페이지 UI 구현
    - 서비스 문구, 서비스명 구현
    - 소셜 로그인(Google, 네이버, 카카오) 버튼 구현

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,5 +1,36 @@
+import Text from '../../components/Text';
+import styles from './index.module.scss';
+import googleLogo from '../../assets/icon/google.svg';
+import kakaoLogo from '../../assets/icon/kakao.svg';
+import naverLogo from '../../assets/icon/naver.svg';
+import { cn } from '../../utils/cn';
+
 const SignIn = () => {
-  return <div>로그인</div>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <header className={styles.serviceInformationContainer}>
+          <Text as="h2" font="heading-small" color="primary">
+            모임을 더 쉽게, 소중한 순간은 더 오래
+          </Text>
+          <Text as="h1" font="display-large" color="brand">
+            HEAR:WE
+          </Text>
+        </header>
+        <div className={styles.signinContainer}>
+          <button className={cn(styles.gooogleSigninButton, styles.signinButton)}>
+            <img src={googleLogo} alt=""></img>Google 로그인
+          </button>
+          <button className={cn(styles.naverSigninButton, styles.signinButton)}>
+            <img src={naverLogo} alt=""></img>네이버 로그인
+          </button>
+          <button className={cn(styles.kakaoSigninButton, styles.signinButton)}>
+            <img src={kakaoLogo} alt=""></img>카카오 로그인
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SignIn;
