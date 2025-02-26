6b300818d5c1235719b8d9346180910327f30bf1
choiseona <tjsdk617@naver.com>
2025-02-27 05:04:45 +0900

    chore: 필요 없는 코드 삭제

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,36 +1,7 @@
 import Header from '@/app/layouts/Header';
-import { TextArea } from '@/shared/ui/TextArea';
-import { useForm, useWatch } from 'react-hook-form';
-
-interface UserInfoType {
-  nickname: string;
-}
 
 function Main() {
-  const { register, control } = useForm<UserInfoType>({
-    defaultValues: {
-      nickname: 'seona',
-    },
-  });
-  const watchedText = useWatch({ control, name: 'nickname' });
-
-  return (
-    <>
-      <Header />
-      <form>
-        <TextArea>
-          <TextArea.Label>라벨</TextArea.Label>
-          <TextArea.Area
-            maxLength={500}
-            placeholder="크루를 소개해주세요(~1000)"
-            hasError={false}
-            {...register('nickname')}
-          />
-          <TextArea.Message variant="information">{watchedText.length}</TextArea.Message>
-        </TextArea>
-      </form>
-    </>
-  );
+  return <Header />;
 }
 
 export default Main;
