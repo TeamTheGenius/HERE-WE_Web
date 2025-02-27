17e31a78cc8b4354b10b2f8d17bc9a3c41246280
choiseona <tjsdk617@naver.com>
2025-02-28 00:51:10 +0900

    feat: 임시 테마 토글 버튼 구현

--- src/app/layouts/Header/index.tsx
+++ src/app/layouts/Header/index.tsx
@@ -3,11 +3,19 @@ import styles from './index.module.scss';
 import ProfileImage from '@/entities/user/ui/ProfileImage';
 import fallbackSource from '@/shared/assets/basic-profile-image-gray.png';
 import Icon from '@/shared/ui/Icon';
+import { useContext } from 'react';
+import { ThemeContext } from '@/shared/contexts/ThemeContext';
+
 function Header() {
+  const themeContext = useContext(ThemeContext);
+
   return (
     <header className={styles.header}>
       <Logo size="md" />
       <div className={styles.headerUserActions}>
+        <button onClick={themeContext?.toggleTheme} className={styles.themeButton}>
+          임시 테마 버튼
+        </button>
         <button>
           <ProfileImage size="small" src={fallbackSource} />
         </button>
