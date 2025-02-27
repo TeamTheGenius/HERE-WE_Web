02c301a9727fcc127a8563afe00068c7ae878bd7
choiseona <tjsdk617@naver.com>
2025-02-28 00:51:10 +0900

    feat: 임시 테마 토글 버튼 구현

--- src/shared/lib/reactChildren.ts
+++ src/shared/lib/reactChildren.ts
@@ -2,5 +2,5 @@ import { Children, isValidElement, ReactNode } from 'react';
 
 export const filterChildrenByComponent = (children: ReactNode, component: JSX.Element['type']) => {
   const childrenArray = Children.toArray(children);
-  return childrenArray.filter((child) => isValidElement(child) && child.type === component).slice(0, 2);
+  return childrenArray.filter((child) => isValidElement(child) && child.type === component);
 };
