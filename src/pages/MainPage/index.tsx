3fbb9579c6cd63db9fd736c78321da53bfbeb988
choiseona <tjsdk617@naver.com>
2025-03-02 22:26:45 +0900

    feat: 다가오는 모먼트 가로 스크롤 적용

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -3,17 +3,28 @@ import styles from './index.module.scss';
 import ParticipatingCrewList from '@/widgets/crew/ParticipatingCrewList';
 import ParticipatingCrewHeader from '@/widgets/crew/ParticipatingCrewHeader';
 import GridContainer from '@/shared/ui/GridContainer';
+import UpcomingMomentList from '@/widgets/moment/UpcomingMomentList';
+import UpcomingMomentHeader from '@/widgets/moment/UpcomingMomentHeader';
+import ScrollableRow from '@/shared/ui/ScrollableRow';
 
 function Main() {
   return (
     <>
       <Header />
-      <main className={styles.mainWrapper}>
-        <ParticipatingCrewHeader />
-        <GridContainer>
-          <ParticipatingCrewList />
-        </GridContainer>
-      </main>
+      <div className={styles.wrapper}>
+        <aside className={styles.container}>
+          <UpcomingMomentHeader />
+          <ScrollableRow>
+            <UpcomingMomentList />
+          </ScrollableRow>
+        </aside>
+        <main className={styles.container}>
+          <ParticipatingCrewHeader />
+          <GridContainer>
+            <ParticipatingCrewList />
+          </GridContainer>
+        </main>
+      </div>
     </>
   );
 }
