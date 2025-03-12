5f479a424cab278328711cd0adffd994afbb8b40
choiseona <tjsdk617@naver.com>
2025-03-13 00:03:22 +0900

    feat: 네비게이션바 컴포넌트 구현
    - 좌측 사이드바 형태의 네비게이션바 구현
    - 모바일 환경에서는 하단 네비게이션바로 전환되는 반응형 디자인 적용
    - 레이아웃 변수 활용
    - URL 파라미터(crewId)를 경로에 동적으로 삽입하는 기능 구현
    - 홈, 크루원, 채팅, 모먼트 메뉴 구성

---
+++ src/app/layouts/NavigationBar/index.tsx
@@ -0,0 +1,23 @@
+import NavitationItem from '@/shared/ui/NavigationItem';
+import styles from './index.module.scss';
+import { useParams } from 'react-router-dom';
+import { routePaths } from '@/app/routes/path';
+
+function NavigationBar() {
+  const { crewId } = useParams();
+
+  const getRoutePath = (url: string) => {
+    return url.replace(/:\w+/, crewId ?? '');
+  };
+
+  return (
+    <nav className={styles.navigationBar}>
+      <NavitationItem icon="home" title="홈" to={getRoutePath(routePaths.home)} />
+      <NavitationItem icon="people" title="크루원" to={getRoutePath(routePaths.member)} />
+      <NavitationItem icon="chat" title="채팅" to={getRoutePath(routePaths.chat)} />
+      <NavitationItem icon="pencil" title="모먼트" to={getRoutePath(routePaths.moment)} />
+    </nav>
+  );
+}
+
+export default NavigationBar;
