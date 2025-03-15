507336c29247de441f37e38839b2e58a3471e3a5
choiseona <tjsdk617@naver.com>
2025-03-15 15:39:30 +0900

    feat: ProfileNickname 폰트 variant 설정

--- src/entities/user/ui/ProfileNickname/index.tsx
+++ src/entities/user/ui/ProfileNickname/index.tsx
@@ -1,6 +1,26 @@
 import { PropsWithChildren } from 'react';
 import styles from './index.module.scss';
+import { cn } from '@/shared/lib/cn';
 
-export function ProfileNickname({ children }: PropsWithChildren) {
-  return <span className={styles.nickname}>{children}</span>;
+interface ProfileNicknameProps extends PropsWithChildren {
+  size: 'lg' | 'md' | 'sm';
+  className?: string;
+}
+
+export function ProfileNickname({ children, size, className }: ProfileNicknameProps) {
+  return (
+    <span
+      className={cn(
+        {
+          [styles.smallNickname]: size === 'sm',
+          [styles.mediumNickname]: size === 'md',
+          [styles.smallNickname]: size === 'sm',
+        },
+        styles.nickname,
+        className,
+      )}
+    >
+      {children}
+    </span>
+  );
 }
