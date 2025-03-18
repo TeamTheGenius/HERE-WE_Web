459854294e1708716c446e7178239269c05f626a
choiseona <tjsdk617@naver.com>
2025-03-18 17:37:39 +0900

    feat: pagination hook 구현
    - usePagination 훅을 구현하여 페이지네이션 기능 제공
    - currentPage 상태를 기반으로 가시적인 페이지 목록(visiblePages) 업데이트
    - 페이지 블록 단위 이동을 위한 로직 구현
    - 페이지 이동 핸들러 구현
    - 전체 페이지, 첫번째 블록, 마지막 블록, 중간 블록을 동적으로 계산하는 함수 구현

---
+++ src/shared/hooks/usePagination.ts
@@ -0,0 +1,83 @@
+import { useEffect, useState } from 'react';
+
+type Page = number | 'nextBlock' | 'prevBlock';
+
+export interface PaginationHookReturn {
+  currentPage: number;
+  visiblePages: Page[];
+  minPage: number;
+  maxPage: number;
+  handleClickPage: (page: number) => void;
+  handleClickPrevPage: () => void;
+  handleClickNextPage: () => void;
+  handleClickPrevPages: () => void;
+  handleClickNextPages: () => void;
+}
+
+export const usePagination = (minPage: number, maxPage: number, blockSize: number = 6): PaginationHookReturn => {
+  const END_BLOCK_SIZE = blockSize - 1;
+  const MIDDLE_BLOCK_SIZE = Math.floor(blockSize / 2);
+
+  const [currentPage, setCurrentPage] = useState(minPage);
+  const [visiblePages, setVisiblePages] = useState<Page[]>([]);
+
+  useEffect(() => {
+    if (currentPage < minPage || currentPage > maxPage) return;
+    setVisiblePages(getBlockPages());
+  }, [currentPage, minPage, maxPage]);
+
+  const getBlockPages = () => {
+    if (maxPage <= blockSize) return getAllPages();
+    if (currentPage <= minPage + MIDDLE_BLOCK_SIZE) return getFirstBlock();
+    else if (currentPage >= maxPage - MIDDLE_BLOCK_SIZE) return getLastBlock();
+    else return getMiddleBlock();
+  };
+
+  const getAllPages = (): Page[] => {
+    // blockSize와 maxPage 같으면 [1,2,3,4,5,6] 반환
+    return Array.from({ length: maxPage - minPage + 1 }, (_, i) => minPage + i);
+  };
+
+  const getFirstBlock = (): Page[] => {
+    // 🔹 minPage 근처 (ex. [1, 2, 3, 4, 5, ..., 51])
+    const pages = Array.from({ length: Math.min(maxPage, END_BLOCK_SIZE) }, (_, i) => minPage + i);
+    if (maxPage - minPage > END_BLOCK_SIZE) return [...pages, 'nextBlock', maxPage];
+    return pages;
+  };
+
+  const getLastBlock = (): Page[] => {
+    // 🔹 maxPage 근처 (ex. [1, ...,47, 48, 49, 50, 51])
+    const pages = Array.from({ length: END_BLOCK_SIZE }, (_, i) => maxPage - END_BLOCK_SIZE + 1 + i);
+    if (maxPage - minPage > END_BLOCK_SIZE) return [minPage, 'prevBlock', ...pages];
+    return pages;
+  };
+
+  const getMiddleBlock = (): Page[] => {
+    // 🔹 중간 페이지 (ex. [1, ..., 9, 10, 11, ..., 51])
+    return [minPage, 'prevBlock', currentPage - 1, currentPage, currentPage + 1, 'nextBlock', maxPage];
+  };
+
+  const handleClickPage = (page: number) => setCurrentPage(page);
+  const handleClickPrevPage = () => setCurrentPage((prev) => Math.max(minPage, prev - 1));
+  const handleClickNextPage = () => setCurrentPage((prev) => Math.min(maxPage, prev + 1));
+  const handleClickPrevPages = () => {
+    if (currentPage > maxPage - END_BLOCK_SIZE + 1) setCurrentPage(Math.max(minPage, maxPage - END_BLOCK_SIZE));
+    else setCurrentPage((prev) => Math.max(minPage, prev - MIDDLE_BLOCK_SIZE));
+  };
+  const handleClickNextPages = () => {
+    if (currentPage < minPage + END_BLOCK_SIZE - 1) setCurrentPage(Math.min(maxPage, minPage + END_BLOCK_SIZE));
+    else setCurrentPage((prev) => Math.min(maxPage, prev + MIDDLE_BLOCK_SIZE));
+  };
+
+  return {
+    currentPage,
+    visiblePages,
+    minPage,
+    maxPage,
+    handleClickPage,
+    handleClickPrevPage,
+    handleClickNextPage,
+    handleClickPrevPages,
+    handleClickNextPages,
+  };
+};
