42c03eb85985e012d3d2a50f0f4d6f05e7d7aaf5
choiseona <tjsdk617@naver.com>
2025-03-18 17:42:11 +0900

    feat: Pagination 컴포넌트 수정
    - usePagination 훅을 활용해 동적으로 페이지 목록 렌더링
    - 현재 페이지(currentPage)에 따라 다음/이전 페이지 이동 버튼 활성화/비활성화

--- src/shared/ui/Pagination/index.tsx
+++ src/shared/ui/Pagination/index.tsx
@@ -1,23 +1,25 @@
 import { PropsWithChildren } from 'react';
 import styles from './index.module.scss';
 import { cn } from '@/shared/lib/cn';
-
-function Main({ children }: PropsWithChildren) {
-  return <ul className={styles.pagination}>{children}</ul>;
-}
+import { PaginationHookReturn } from '@/shared/hooks/usePagination';
 
 interface PaginationButtonProps extends PropsWithChildren {
   isActive?: boolean;
+  isDisAbled?: boolean;
+  handleClick?: () => void;
 }
 
-function PaginationButton({ isActive = false, children }: PaginationButtonProps) {
+function PaginationButton({ isActive = false, isDisAbled = false, children, handleClick }: PaginationButtonProps) {
   return (
     <li>
       <button
+        disabled={isDisAbled}
+        onClick={handleClick}
         className={cn(
           {
-            [styles.paginationButtonActive]: isActive === true,
-            [styles.paginationButtonInactive]: isActive === false,
+            [styles.paginationButtonActive]: isActive && !isDisAbled,
+            [styles.paginationButtonInactive]: !isActive && !isDisAbled,
+            [styles.paginationButtonDisabled]: isDisAbled,
           },
           styles.paginationButton,
         )}
@@ -28,6 +30,66 @@ function PaginationButton({ isActive = false, children }: PaginationButtonProps)
   );
 }
 
-export const Pagination = Object.assign(Main, {
-  Button: PaginationButton,
-});
+interface PaginationProps {
+  nextBlockChar?: string;
+  prevBlockChar?: string;
+  nextPageChar?: string;
+  prevPageChar?: string;
+  blockSize?: number;
+  paginationTools: PaginationHookReturn;
+}
+
+function Pagination({
+  nextBlockChar = '...',
+  prevBlockChar = '...',
+  nextPageChar = '>',
+  prevPageChar = '<',
+  paginationTools,
+}: PaginationProps) {
+  const {
+    visiblePages,
+    currentPage,
+    minPage,
+    maxPage,
+    handleClickPrevPage,
+    handleClickPrevPages,
+    handleClickNextPage,
+    handleClickNextPages,
+    handleClickPage,
+  } = paginationTools;
+
+  return (
+    <ul className={styles.pagination}>
+      <PaginationButton key="prevPage" isDisAbled={currentPage === minPage} handleClick={handleClickPrevPage}>
+        {prevPageChar}
+      </PaginationButton>
+
+      {visiblePages.map((page) => {
+        if (page === 'prevBlock')
+          return (
+            <PaginationButton key="prevBlock" handleClick={handleClickPrevPages}>
+              {prevBlockChar}
+            </PaginationButton>
+          );
+        else if (page === 'nextBlock') {
+          return (
+            <PaginationButton key="nextBlock" handleClick={handleClickNextPages}>
+              {nextBlockChar}
+            </PaginationButton>
+          );
+        } else
+          return (
+            <PaginationButton isActive={page === currentPage} handleClick={() => handleClickPage(page)} key={page}>
+              {page}
+            </PaginationButton>
+          );
+      })}
+
+      <PaginationButton key="nextPage" isDisAbled={currentPage === maxPage} handleClick={handleClickNextPage}>
+        {nextPageChar}
+      </PaginationButton>
+    </ul>
+  );
+}
+
+export default Pagination;
