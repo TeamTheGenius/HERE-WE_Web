10e68d220c9e501174542ad8b8dca9f586c00d64
choiseona <tjsdk617@naver.com>
2025-03-19 23:54:25 +0900

    feat: 크루 생성 페이지 api 연동
    - usePostCrew와 usePostCrewFile을 이용한 크루 생성 api 연동
    - postCrew를 통해 얻은 crewId를 통해 crewFile 업로드

--- src/widgets/crew/ParticipatingCrewHeader/index.tsx
+++ src/widgets/crew/ParticipatingCrewHeader/index.tsx
@@ -1,11 +1,19 @@
 import Button from '@/shared/ui/Button';
 import styles from './index.module.scss';
+import { useNavigate } from 'react-router-dom';
+import { routePaths } from '@/app/routes/path';
 
 function ParticipatingCrewHeader() {
+  const navigate = useNavigate();
+
+  const handleClickCreateCrew = () => {
+    navigate(routePaths.createCrew);
+  };
+
   return (
     <div className={styles.container}>
       <h2 className={styles.title}>참여중인 크루</h2>
-      <Button>+ 크루 생성</Button>
+      <Button onClick={handleClickCreateCrew}>+ 크루 생성</Button>
     </div>
   );
 }
