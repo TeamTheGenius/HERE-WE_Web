7e396bc0ce082ec09f43c7d6f31573ca4a0aadd8
choiseona <tjsdk617@naver.com>
2025-03-23 12:36:14 +0900

    feat: 크루원 초대 모달 UI 구현

--- src/widgets/member/CrewInviteButton/index.tsx
+++ src/widgets/member/CrewInviteButton/index.tsx
@@ -1,8 +1,18 @@
 import Button from '@/shared/ui/Button';
 import styles from './index.module.scss';
+import { useModal } from '@/shared/hooks/useModal';
+import CrewInviteModal from '@/features/member/ui/MemberInviteModal';
 
 function CrewInviteButton() {
-  return <Button className={styles.inviteButton}>+ 초대</Button>;
+  const { openModal, closeModal, isOpen } = useModal();
+  return (
+    <>
+      <CrewInviteModal handleClose={closeModal} isOpen={isOpen} />
+      <Button className={styles.inviteButton} onClick={openModal}>
+        + 초대
+      </Button>
+    </>
+  );
 }
 
 export default CrewInviteButton;
