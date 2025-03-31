6afe4723c019d447b55e7c3482b9a5cd669578fb
choiseona <tjsdk617@naver.com>
2025-03-31 15:50:53 +0900

    feat: 모먼트 제목 UI 추가

--- src/pages/MomentPlacePage/index.tsx
+++ src/pages/MomentPlacePage/index.tsx
@@ -14,31 +14,34 @@ function MomentPlacePage() {
   };
 
   return (
-    <main className={styles.container}>
-      <div className={styles.locationSection}>
-        <ul className={styles.tabs}>
-          <li onClick={() => setCurrentTab('검색')} className={styles.tabItem}>
-            <button>검색</button>
-          </li>
-          <li className={styles.tabItem}>
-            <button onClick={() => setCurrentTab('방문 장소')}>방문 장소</button>
-          </li>
-        </ul>
-        <div className={styles.tabContent}>
-          {currentTab === '검색' && (
-            <LocationSearchForm
-              keyword={keyword}
-              handleSubmitKeyword={handleSubmitKeyword}
-              handleSelectLocation={() => {}}
-            />
-          )}
-          {currentTab === '방문 장소' && <div>방문 장소</div>}
+    <div className={styles.container}>
+      <h2 className={styles.title}>피자 먹으러 가자</h2>
+      <main className={styles.contentContainer}>
+        <div className={styles.locationSection}>
+          <ul className={styles.tabs}>
+            <li onClick={() => setCurrentTab('검색')} className={styles.tabItem}>
+              <button>검색</button>
+            </li>
+            <li className={styles.tabItem}>
+              <button onClick={() => setCurrentTab('방문 장소')}>방문 장소</button>
+            </li>
+          </ul>
+          <div className={styles.tabContent}>
+            {currentTab === '검색' && (
+              <LocationSearchForm
+                keyword={keyword}
+                handleSubmitKeyword={handleSubmitKeyword}
+                handleSelectLocation={() => {}}
+              />
+            )}
+            {currentTab === '방문 장소' && <div>방문 장소</div>}
+          </div>
         </div>
-      </div>
-      <div className={styles.mapWrapper}>
-        <Map />
-      </div>
-    </main>
+        <div className={styles.mapWrapper}>
+          <Map />
+        </div>
+      </main>
+    </div>
   );
 }
 
