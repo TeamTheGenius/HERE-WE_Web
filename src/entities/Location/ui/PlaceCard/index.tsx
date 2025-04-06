00c11350c27921c727465b2adaaf00fc7549b8d1
choiseona <tjsdk617@naver.com>
2025-04-06 13:48:18 +0900

    feat: PlaceCard 컴포넌트 및 서브 컴포넌트 구현 >> >> - as prop을 활용하여 Body 컴포넌트가 div 또는 button으로 렌더링 가능하게 구현 >> - button 타입일 경우에만 클릭 이벤트 바인딩

---
+++ src/entities/Location/ui/PlaceCard/index.tsx
@@ -0,0 +1,49 @@
+import { PropsWithChildren } from 'react';
+import styles from './index.module.scss';
+
+interface PlaceBodyProps extends PropsWithChildren {
+  as?: 'div' | 'button';
+  handleClick?: () => void;
+}
+
+interface PlaceButtonProps extends PropsWithChildren {
+  handleClick: () => void;
+}
+
+function PlaceHeader({ children }: PropsWithChildren) {
+  return <div className={styles.header}>{children}</div>;
+}
+
+function PlaceTitle({ children }: PropsWithChildren) {
+  return <h3 className={styles.title}>{children}</h3>;
+}
+
+function PlaceButton({ children }: PlaceButtonProps) {
+  return <button className={styles.button}>{children}</button>;
+}
+
+function PlaceBody({ as: Component = 'button', handleClick, children }: PlaceBodyProps) {
+  const isButton = Component === 'button';
+
+  return (
+    <Component className={styles.body} {...(isButton && handleClick ? { onClick: handleClick } : {})}>
+      {children}
+    </Component>
+  );
+}
+
+function PlaceDetail({ children }: PropsWithChildren) {
+  return <p className={styles.detail}>{children}</p>;
+}
+
+function Main({ children }: PropsWithChildren) {
+  return <div className={styles.wrapper}>{children}</div>;
+}
+
+export const PlaceCard = Object.assign(Main, {
+  Header: PlaceHeader,
+  Body: PlaceBody,
+  Title: PlaceTitle,
+  Button: PlaceButton,
+  Detail: PlaceDetail,
+});
--- src/entities/moment/ui/MomentPlaceHeader/index.tsx
+++ src/entities/moment/ui/MomentPlaceHeader/index.tsx
@@ -1,11 +1,22 @@
 import Button from '@/shared/ui/Button';
 import styles from './index.module.scss';
+import { useNavigate, useParams } from 'react-router-dom';
+import { routePaths } from '@/app/routes/path';
 
 function MomentPlaceHeader() {
+  const navigate = useNavigate();
+  const { crewId, momentId } = useParams();
+
+  const handleClickMorePlaces = () => {
+    navigate(routePaths.momentPlace.getPath(Number(crewId), Number(momentId)));
+  };
+
   return (
     <div className={styles.container}>
       <h2 className={styles.title}>방문 장소</h2>
-      <Button variant="secondary">더보기</Button>
+      <Button onClick={handleClickMorePlaces} variant="secondary">
+        더보기
+      </Button>
     </div>
   );
 }
