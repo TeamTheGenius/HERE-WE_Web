231ef1f0f4f6e0aa6f5f9f5880dbd5e4a47ddede
choiseona <tjsdk617@naver.com>
2025-04-11 16:43:48 +0900

    feat: 모먼트 참여자 목록 api 연동
    - api 연동
    - 무한 스크롤 적용

--- src/entities/user/model/types.ts
+++ src/entities/user/model/types.ts
@@ -2,3 +2,8 @@ export interface UserInfoType {
   image: FileList;
   nickname: string;
 }
+
+export interface UserJson {
+  name: string;
+  userId: number;
+}
--- src/features/moment/query/momentFeatureQueries.ts
+++ src/features/moment/query/momentFeatureQueries.ts
@@ -1,20 +1,35 @@
-import { Pagination } from '@/shared/types/api';
+import { InfiniteScroll, Pagination } from '@/shared/types/api';
 import { getCrewMomentList, GetCrewMomentListRequest } from '../api/getCrewMomentList';
-import { queryOptions } from '@tanstack/react-query';
+import { infiniteQueryOptions, QueryFunctionContext, queryOptions } from '@tanstack/react-query';
 import { MomentJSONType } from '@/entities/moment/model/types';
 import { momentQueries } from '@/entities/moment/query/momentQueries';
 import { getMomentPlaces, GetMomentPlacesRequest } from '../api/getMomentPlaces';
 import { getUpcomingMoments, GetUpcomingMomentsRequest } from '../api/getUpcomingMoments';
+import { getMomentParticipantList, GetMomentParticipantListRequest } from '../api/getMomentParticipantList';
+import { UserJson } from '@/entities/user/model/types';
 
 export const momentFeatureQueries = {
   allListKeys: [...momentQueries.allKeys, 'list'] as const,
   allPlacesKeys: [...momentQueries.allKeys, 'place'] as const,
+  allParticipantKeys: [...momentQueries.allKeys, 'participant'] as const,
+
+  participantsInfiniteJSON: ({ page, size, momentId }: GetMomentParticipantListRequest) =>
+    infiniteQueryOptions<InfiniteScroll<UserJson>>({
+      queryKey: [...momentFeatureQueries.allParticipantKeys, momentId, page, size],
+      queryFn: async ({ pageParam }: QueryFunctionContext) => {
+        const currentPage = typeof pageParam === 'number' ? pageParam : 1;
+        return await getMomentParticipantList({ page: currentPage, size, momentId });
+      },
+      initialPageParam: page,
+      getNextPageParam: (lastPage) => (lastPage.last ? undefined : lastPage.number + 1),
+    }),
 
   crewMomentPaginationJSON: ({ page, size, crewId }: GetCrewMomentListRequest) =>
     queryOptions<Pagination<MomentJSONType>>({
       queryKey: [...momentFeatureQueries.allListKeys, crewId, page, size],
       queryFn: () => getCrewMomentList({ crewId, page, size }),
     }),
+
   upcomingMomentsJSON: ({ page, size }: GetUpcomingMomentsRequest) =>
     queryOptions({
       queryKey: [...momentFeatureQueries.allListKeys, 'json', page, size],
