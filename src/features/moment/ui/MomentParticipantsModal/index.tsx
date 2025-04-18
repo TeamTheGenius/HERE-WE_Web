ac925448d326bc55d2d80714a4965a85d67b4476
choiseona <tjsdk617@naver.com>
2025-04-18 15:05:04 +0900

    feat: 모먼트 참여 크루원 없음 상태 UI 구현
    - 모먼트 참여 크루원이 없응ㄹ 때 표시할 EmptyState 컴포넌트 적용
    - 참여 유도 문구 포함

--- src/features/moment/ui/MomentParticipantsModal/index.tsx
+++ src/features/moment/ui/MomentParticipantsModal/index.tsx
@@ -6,6 +6,7 @@ import { useInfiniteScroll } from '@/shared/hooks/useInfiniteScroll';
 import { useInfiniteQuery } from '@tanstack/react-query';
 import { momentFeatureQueries } from '../../query/momentFeatureQueries';
 import ProfileImageByUserId from '@/entities/user/ui/ProfileImageByUserId';
+import { EmptyState } from '@/shared/ui/EmptyState';
 interface MomentParticipantsModalProps {
   isOpen: boolean;
   closeModal: () => void;
@@ -30,19 +31,27 @@ function MomentParticipantsModal({ isOpen, closeModal }: MomentParticipantsModal
   return (
     <Modal isOpen={isOpen}>
       <Modal.Overlay handleClick={closeModal} />
-      <Modal.Title>참여자 목록</Modal.Title>
+      <Modal.Title>모먼트 참여자 목록</Modal.Title>
       <Modal.Content>
-        <ul className={styles.participantList} ref={rootRef}>
-          {data.pages.map((page) =>
-            page.content.map((user) => (
-              <li key={user.userId} className={styles.participnatItem}>
-                <ProfileImageByUserId size="medium" userId={user.userId} alt="프로필 이미지" />
-                <ProfileNickname size="md">{user.name}</ProfileNickname>
-              </li>
-            )),
-          )}
-          <div style={{ height: '1px' }} ref={targetRef} />
-        </ul>
+        {!data?.pages?.[0]?.content?.length ? (
+          <EmptyState>
+            <EmptyState.Icon icon="people" iconSize="80" />
+            <EmptyState.Description>아직 참여한 크루원이 없습니다</EmptyState.Description>
+            <EmptyState.Description>첫 번째 참여자가 되어보세요!</EmptyState.Description>
+          </EmptyState>
+        ) : (
+          <ul className={styles.participantList} ref={rootRef}>
+            {data.pages.map((page) =>
+              page.content.map((user) => (
+                <li key={user.userId} className={styles.participnatItem}>
+                  <ProfileImageByUserId size="medium" userId={user.userId} alt="프로필 이미지" />
+                  <ProfileNickname size="md">{user.name}</ProfileNickname>
+                </li>
+              )),
+            )}
+            <div style={{ height: '1px' }} ref={targetRef} />
+          </ul>
+        )}
       </Modal.Content>
       <Modal.RightButton onClick={closeModal}>확인</Modal.RightButton>
     </Modal>
