729d77d4bf48b304473d95072504c6f657f0f7b5
choiseona <tjsdk617@naver.com>
2025-04-18 15:06:18 +0900

    feat: 다가오는 모먼트 없음 상태 UI 구현
    - 다가오는 모먼트 없을 때 표시할 EmptyState 컴포넌트 적용
    - 참여 유도 문구 포함

--- src/pages/UpcomingMomentsPage/index.tsx
+++ src/pages/UpcomingMomentsPage/index.tsx
@@ -1,6 +1,7 @@
 import { useUpcomingMomentsWithFile } from '@/features/moment/query/useUpcomingMomentsWithFile';
 import UpcomingMomentCard from '@/features/moment/ui/UpcomingMomentCard';
 import { usePagination } from '@/shared/hooks/usePagination';
+import { EmptyState } from '@/shared/ui/EmptyState';
 import GridContainer from '@/shared/ui/GridContainer';
 import { Pagination } from '@/shared/ui/Pagination';
 import { TitledLayout } from '@/shared/ui/TitledLayout';
@@ -15,24 +16,28 @@ function UpcomingMomentsPage() {
     if (upcomingMomentsData?.page?.totalPages) setMaxPage(upcomingMomentsData.page.totalPages);
   }, [upcomingMomentsData?.page?.totalPages, setMaxPage]);
 
-  if (!upcomingMomentsData) return null;
-  const { content } = upcomingMomentsData;
-
   return (
     <TitledLayout>
       <TitledLayout.Title>다가오는 모먼트</TitledLayout.Title>
       <TitledLayout.Content>
+        {(!upcomingMomentsData || !upcomingMomentsData?.content.length) && (
+          <EmptyState>
+            <EmptyState.Icon icon="calendar" iconSize="80" />
+            <EmptyState.Description>다가오는 모먼트가 없습니다</EmptyState.Description>
+            <EmptyState.Description>크루의 모먼트에 참여해보세요!</EmptyState.Description>
+          </EmptyState>
+        )}
         <Pagination>
           <Pagination.Content>
             <GridContainer>
-              {content.map((moment) => (
+              {upcomingMomentsData?.content.map((moment) => (
                 <UpcomingMomentCard key={moment.momentId} size="full" data={moment} />
               ))}
             </GridContainer>
           </Pagination.Content>
           <Pagination.Controller
             paginationTools={paginationTools}
-            isVisible={upcomingMomentsData.page.totalPages > 0}
+            isVisible={(upcomingMomentsData?.page?.totalPages ?? 0) > 0}
           ></Pagination.Controller>
         </Pagination>
       </TitledLayout.Content>
