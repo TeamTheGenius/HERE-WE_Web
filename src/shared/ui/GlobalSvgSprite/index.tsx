14b4684d4ec5be271457094ccfce0dabfffdbce3
choiseona <tjsdk617@naver.com>
2025-04-26 02:14:17 +0900

    feat: 모먼트 썸네일 스타일 수정
    - 이미지가 컨테이너를 완전히 덮고, 중앙에 위치하도록 스타일 수정

--- src/shared/ui/GlobalSvgSprite/index.tsx
+++ src/shared/ui/GlobalSvgSprite/index.tsx
@@ -159,24 +159,24 @@ const svgSpriteCode = (
     </symbol>
     <symbol id="leave" viewBox="0 0 20 20">
       <path
-        fill-rule="evenodd"
+        fillRule="evenodd"
         d="M15.347 7.116a.5.5 0 0 1 .704.064l2.083 2.5a.5.5 0 0 1-.768.64l-2.083-2.5a.5.5 0 0 1 .064-.704z"
-        clip-rule="evenodd"
+        clipRule="evenodd"
       />
       <path
-        fill-rule="evenodd"
+        fillRule="evenodd"
         d="M15.347 12.884a.5.5 0 0 1-.064-.704l2.083-2.5a.5.5 0 1 1 .768.64l-2.083 2.5a.5.5 0 0 1-.704.064z"
-        clip-rule="evenodd"
+        clipRule="evenodd"
       />
       <path
-        fill-rule="evenodd"
+        fillRule="evenodd"
         d="M17.5 10a.5.5 0 0 1-.5.5H9.5a.5.5 0 0 1 0-1H17a.5.5 0 0 1 .5.5zm-14-7a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.5-.5zm0 14a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.5-.5z"
-        clip-rule="evenodd"
+        clipRule="evenodd"
       />
       <path
-        fill-rule="evenodd"
+        fillRule="evenodd"
         d="M13 2.5a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V3a.5.5 0 0 1 .5-.5zm0 10a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0v-4a.5.5 0 0 1 .5-.5zm-9-10a.5.5 0 0 1 .5.5v14a.5.5 0 0 1-1 0V3a.5.5 0 0 1 .5-.5z"
-        clip-rule="evenodd"
+        clipRule="evenodd"
       />
     </symbol>
   </svg>
