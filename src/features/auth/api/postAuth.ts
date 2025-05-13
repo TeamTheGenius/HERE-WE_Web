6342646511f476de84ce31897d9d05bf3aaea620
choiseona <tjsdk617@naver.com>
2025-05-13 18:53:32 +0900

    fix: 사용자 인가 요청시 token과 userId 담아서 api 요청

--- src/features/auth/api/postAuth.ts
+++ src/features/auth/api/postAuth.ts
@@ -1,9 +1,10 @@
 import { privateClient } from '@/shared/api/config';
 import { formatImageSource } from '@/shared/helper/formatImageSource';
 
-export const postAuth = async (userId: number) => {
+export const postAuth = async (userId: number, token: string) => {
   const { data: response } = await privateClient.post(`auth`, {
     userId: userId,
+    token: token,
   });
 
   const { nickname, fileResponse } = response.data;
