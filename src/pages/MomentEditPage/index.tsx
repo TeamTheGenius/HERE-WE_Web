36e9772ba45cd68551e35c843023e02bdca2d0af
choiseona <tjsdk617@naver.com>
2025-08-05 14:03:54 +0900

    feat: 모먼트 수정 페이지 스켈레톤 적용

--- src/pages/MomentEditPage/index.tsx
+++ src/pages/MomentEditPage/index.tsx
@@ -1,56 +1,69 @@
-import { MomentJSONMutationRequest } from '@/entities/moment/model/types';
-import { useMomentDetailWithFile } from '@/entities/moment/query/useMomentDetailWithFile';
+import { routePaths } from '@/app/routes/path';
 import { usePatchMoment } from '@/entities/moment/query/usePatchMoment';
 import { usePatchMomentFile } from '@/entities/moment/query/usePatchMomentFile';
+import MomentEditContent from '@/entities/moment/ui/MomentEditContent';
+import { useMomentRegister } from '@/features/moment/hook/useMomentRegister';
 import { MomentFormType } from '@/features/moment/model/types';
-import MomentForm from '@/features/moment/ui/MomentForm';
-import { makeSourceToFileList } from '@/shared/helper/sourceToFileList';
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { formatLocalDateTime } from '@/shared/lib/dateFormater';
+import DetailListSkeleton from '@/shared/ui/DetailListSkeleton';
+import { TitledFormLayout } from '@/shared/ui/TitledFormLayout';
+import { Suspense } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 
 function MomentEditPage() {
-  const { momentId } = useParams();
+  const { momentId, crewId } = useParams();
+  const navigate = useNavigate();
+
   const { mutateAsync: patchMoment } = usePatchMoment();
   const { mutateAsync: patchMomentFile } = usePatchMomentFile();
-  const { data: momentDetail } = useMomentDetailWithFile(Number(momentId));
-  const [initialFileList, setInitialFileList] = useState<FileList | null>(null);
 
-  useEffect(() => {
-    const file = momentDetail?.file;
-    if (!file) return;
+  const register = useMomentRegister({
+    name: '',
+    capacity: undefined,
+    meetAt: '',
+    closedAt: '',
+    place: undefined,
+    image: undefined,
+  });
 
-    const fetchData = async () => {
-      const fileList = await makeSourceToFileList(file.source, 'moment-image', file.fileEnv);
-      if (fileList) setInitialFileList(fileList);
-    };
-    fetchData();
-  }, [momentDetail?.file]);
+  const handleSubmit = async (data: MomentFormType) => {
+    const { name, image, capacity, closedAt, meetAt, place } = data;
+    if (!crewId || !capacity || !place) return;
+    const files = image ? [...image] : [];
 
-  if (!momentDetail || !initialFileList) return null;
+    const closedAtDate = new Date(closedAt);
+    const meetAtDate = new Date(meetAt);
+    const formattedClosedAt = formatLocalDateTime(closedAtDate);
+    const formattedMeetAt = formatLocalDateTime(meetAtDate);
 
-  const { name, capacity, meetAt, closedAt, place } = momentDetail;
-
-  const initialData: MomentFormType = {
-    name,
-    capacity,
-    meetAt,
-    closedAt,
-    place,
-    image: initialFileList,
-  };
-
-  const handleJSONSubmit = async (data: MomentJSONMutationRequest) => {
-    await patchMoment({ momentId: Number(momentId), ...data });
-    return { momentId: Number(momentId) };
+    try {
+      await Promise.all([
+        patchMoment({
+          momentId: Number(momentId),
+          momentName: name,
+          place: place,
+          closedAt: formattedClosedAt,
+          meetAt: formattedMeetAt,
+          capacity: capacity,
+        }),
+        patchMomentFile({ id: Number(momentId), files: files }),
+      ]);
+      navigate(routePaths.momentDetail.getPath(Number(crewId), Number(momentId)));
+    } catch (error) {
+      register.handleApiError(error);
+    }
   };
 
   return (
-    <MomentForm
-      initialData={initialData}
-      handleJSONSubmit={handleJSONSubmit}
-      handleFIleSUbmit={patchMomentFile}
-      submitType="수정"
-    />
+    <TitledFormLayout>
+      <TitledFormLayout.Title>모먼트 수정 페이지</TitledFormLayout.Title>
+      <TitledFormLayout.Form handleSubmit={register.formMethods.handleSubmit(handleSubmit)}>
+        <Suspense fallback={<DetailListSkeleton itemCount={6} titleWidth="12rem" contentHeight="6rem" />}>
+          <MomentEditContent momentRegister={register} momentId={Number(momentId)} crewId={Number(crewId)} />
+        </Suspense>
+        <TitledFormLayout.Button>수정하기</TitledFormLayout.Button>
+      </TitledFormLayout.Form>
+    </TitledFormLayout>
   );
 }
 
