bf6c15295602b108ed69f0ed85cc78c4ab1628ff
choiseona <tjsdk617@naver.com>
2025-08-05 13:55:21 +0900

    feat: 크루 생성 페이지 스켈레톤 적용

--- src/pages/CrewCreatePage/index.tsx
+++ src/pages/CrewCreatePage/index.tsx
@@ -1,29 +1,41 @@
-import { CrewJSONMutationRequest } from '@/entities/crew/model/types';
+import { routePaths } from '@/app/routes/path';
 import { usePostCrew } from '@/entities/crew/query/usePostCrew';
 import { usePostCrewFile } from '@/entities/crew/query/usePostCrewFile';
-import CrewForm from '@/features/crew/ui/CrewForm';
-import { FileMutationRequest } from '@/shared/types/api';
+import CrewFormField from '@/entities/crew/ui/CrewFormField';
+import { CrewFormType } from '@/features/crew/model/types';
+import { useCrewRegister } from '@/features/crew/model/useCrewRegister';
+import { TitledFormLayout } from '@/shared/ui/TitledFormLayout';
+import { useNavigate } from 'react-router-dom';
 
 function CrewCreatePage() {
+  const navigate = useNavigate();
   const { mutateAsync: postCrew } = usePostCrew();
   const { mutateAsync: postCrewFile } = usePostCrewFile();
 
-  const handleJSONSubmit = async (data: CrewJSONMutationRequest) => {
-    const { crewId: returnedMomentId } = await postCrew({ ...data });
-    return { crewId: returnedMomentId };
+  const handleSubmit = async (data: CrewFormType) => {
+    const { title, introduce, image } = data;
+    const files = image ? [...image] : [];
+
+    const { crewId } = await postCrew({ name: title, introduce: introduce });
+    await postCrewFile({ id: crewId, files: files });
+
+    navigate(routePaths.home.getPath(crewId));
   };
 
-  const handleFileSubmit = async (data: FileMutationRequest) => {
-    await postCrewFile({ ...data });
-  };
+  const crewRegister = useCrewRegister({
+    image: undefined,
+    introduce: '',
+    title: '',
+  });
 
   return (
-    <CrewForm
-      initialData={{ image: undefined, introduce: '', title: '' }}
-      handleJSONSubmit={handleJSONSubmit}
-      handleFIleSubmit={handleFileSubmit}
-      submitType="생성"
-    />
+    <TitledFormLayout>
+      <TitledFormLayout.Title>크루 생성 페이지</TitledFormLayout.Title>
+      <TitledFormLayout.Form handleSubmit={crewRegister.formMethods.handleSubmit(handleSubmit)}>
+        <CrewFormField crewRegister={crewRegister} />
+        <TitledFormLayout.Button>생성하기</TitledFormLayout.Button>
+      </TitledFormLayout.Form>
+    </TitledFormLayout>
   );
 }
 
--- src/shared/ui/TitledFormLayout/index.tsx
+++ src/shared/ui/TitledFormLayout/index.tsx
@@ -1,44 +1,31 @@
 import { FormEvent, PropsWithChildren } from 'react';
 import styles from './index.module.scss';
 import Button from '../Button';
-import { filterChildrenByComponent } from '@/shared/lib/reactChildren';
 
-interface MainProps extends PropsWithChildren {
+interface TitledFormLayoutFormProps extends PropsWithChildren {
   handleSubmit: (e: FormEvent<HTMLFormElement>) => void;
 }
 
-const TitleComponent = (<TitledFormLayoutTitle />).type;
-const FormComponent = (<TitledFormLayoutForm />).type;
-const ButtonComponent = (<TitledFormLayoutButton />).type;
-
-function Main({ children, handleSubmit }: MainProps) {
-  const titleElement = filterChildrenByComponent(children, TitleComponent);
-  const formElement = filterChildrenByComponent(children, FormComponent);
-  const buttonElement = filterChildrenByComponent(children, ButtonComponent);
-
-  return (
-    <main className={styles.container}>
-      {titleElement}
-      <form onSubmit={handleSubmit} className={styles.form}>
-        {formElement}
-        {buttonElement}
-      </form>
-    </main>
-  );
+function Main({ children }: PropsWithChildren) {
+  return <main className={styles.container}>{children}</main>;
 }
 
 function TitledFormLayoutTitle({ children }: PropsWithChildren) {
   return <h2 className={styles.title}>{children}</h2>;
 }
 
-function TitledFormLayoutForm({ children }: PropsWithChildren) {
-  return <>{children}</>;
+function TitledFormLayoutForm({ children, handleSubmit }: TitledFormLayoutFormProps) {
+  return (
+    <form onSubmit={handleSubmit} className={styles.form}>
+      {children}
+    </form>
+  );
 }
 
 function TitledFormLayoutButton({ children }: PropsWithChildren) {
   return (
     <div className={styles.submitButtonWrapper}>
-      <Button size="lg" className={styles.submitButton}>
+      <Button type="submit" size="lg" className={styles.submitButton}>
         {children}
       </Button>
     </div>
