56f7034da02b2995e1d3feebc3adb5509496400e
choiseona <tjsdk617@naver.com>
2025-08-05 13:58:52 +0900

    feat: 멤버 페이지 스켈레톤 적용

--- src/pages/MemberPage/index.tsx
+++ src/pages/MemberPage/index.tsx
@@ -2,15 +2,22 @@ import BaseCrewHeader from '@/widgets/crew/BaseCrewHeader';
 import MemberListSection from '@/widgets/member/MemberListSection';
 import CrewInviteButton from '@/widgets/member/CrewInviteButton';
 import styles from './index.module.scss';
+import Skeleton from '@/shared/ui/Skeleton';
+import MemberListSkeleton from '@/entities/member/ui/MemberListSkeleton';
+import { Suspense } from 'react';
 
 function CrewMembersPage() {
   return (
     <div className={styles.wapper}>
-      <BaseCrewHeader />
+      <Suspense fallback={<Skeleton width="10rem" height="2rem" variant="rect" />}>
+        <BaseCrewHeader />
+      </Suspense>
       <div>
         <CrewInviteButton />
       </div>
-      <MemberListSection />
+      <Suspense fallback={<MemberListSkeleton />}>
+        <MemberListSection />
+      </Suspense>
     </div>
   );
 }
