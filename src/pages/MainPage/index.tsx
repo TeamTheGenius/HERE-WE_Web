c0a0255991dadf49b22858284a7c572946c98b8f
choiseona <tjsdk617@naver.com>
2025-08-05 13:51:19 +0900

    feat: MainPage 스켈레톤 적용

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,19 +1,25 @@
+import ParticipatingCrewHeader from '@/features/crew/ui/ParticipatingCrewHeader';
 import styles from './index.module.scss';
-import ParticipatingCrewList from '@/widgets/crew/ParticipatingCrewList';
-import ParticipatingCrewHeader from '@/widgets/crew/ParticipatingCrewHeader';
-import UpcomingMomentHeader from '@/widgets/moment/UpcomingMomentHeader';
-import UpcomingMomentsRow from '@/widgets/moment/UpcomingMomentsRow';
+import UpcomingMomentHeader from '@/features/moment/ui/UpcomingMomentHeader';
+import UpcomingMomentsRow from '@/features/moment/ui/UpcomingMomentsRow';
+import ParticipatingCrewList from '@/features/crew/ui/ParticipatingCrewList';
+import { Suspense } from 'react';
+import CardListSkeleton from '@/shared/ui/CardListSkeleton';
 
 function Main() {
   return (
     <div className={styles.wrapper}>
       <aside className={styles.container}>
         <UpcomingMomentHeader />
-        <UpcomingMomentsRow />
+        <Suspense fallback={<CardListSkeleton direction="horizontal" />}>
+          <UpcomingMomentsRow />
+        </Suspense>
       </aside>
       <main className={styles.listContainer}>
         <ParticipatingCrewHeader />
-        <ParticipatingCrewList />
+        <Suspense fallback={<CardListSkeleton direction="grid" />}>
+          <ParticipatingCrewList />
+        </Suspense>
       </main>
     </div>
   );
