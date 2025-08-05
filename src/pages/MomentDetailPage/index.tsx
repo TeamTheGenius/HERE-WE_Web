8f72826ac3660248e831846365ff6f6063ed6b09
choiseona <tjsdk617@naver.com>
2025-08-05 14:05:13 +0900

    모먼트 세부사항 페이지 스켈레톤 적용

--- src/pages/MomentDetailPage/index.tsx
+++ src/pages/MomentDetailPage/index.tsx
@@ -3,17 +3,24 @@ import MomentInformation from '@/entities/moment/ui/MomentInformation';
 import MomentPlaceHeader from '@/entities/moment/ui/MomentPlaceHeader';
 import MomentPlaceRow from '@/features/moment/ui/MomentPlaceRow';
 import styles from './index.module.scss';
+import MomentInformationSkeleton from '@/entities/moment/ui/MomentInformationSkeleton';
+import CardListSkeleton from '@/shared/ui/CardListSkeleton';
+import { Suspense } from 'react';
 
 function MomentDetailPage() {
   return (
     <main className={styles.container}>
       <section>
         <MomentDetailHeader />
-        <MomentInformation />
+        <Suspense fallback={<MomentInformationSkeleton />}>
+          <MomentInformation />
+        </Suspense>
       </section>
       <section>
         <MomentPlaceHeader />
-        <MomentPlaceRow />
+        <Suspense fallback={<CardListSkeleton direction="horizontal" />}>
+          <MomentPlaceRow />
+        </Suspense>
       </section>
     </main>
   );
